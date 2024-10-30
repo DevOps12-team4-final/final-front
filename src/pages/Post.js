280329a9e425e3be4fd7a38a846b92fb75de2ae2
sseopsseop <ponde119@outlook.com>
2024-10-30 18:03:17 +0900

    24.10.30 이준섭 post 페이지 작업중

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,13 +1,21 @@
 import { useLocation } from 'react-router-dom';
+import Header from './frame/Header';
+import Footer from './frame/Footer';
+
+import '../scss/Post.scss';
 
 function Post() {
   const location = useLocation();
   const user = location.state; // user 데이터를 state로부터 가져오기
 
   return (
-    <div>
-      <h1>게시물 페이지</h1>
-      {user && <p>{user.nickname}님, 환영합니다!</p>}
+    <div id='post'>
+      <div className='post_container'>
+            <Header/>
+            <h1>게시물 페이지</h1>
+            {user && <p>{user.nickname}님, 환영합니다!</p>}
+            <Footer/>
+      </div>
     </div>
   );
 }
